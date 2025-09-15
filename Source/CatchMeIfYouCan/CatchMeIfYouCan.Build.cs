b0a08dddeb17fd7b4caabfca07594e4b3caaf037
kmy9317 <kimmin9632@gmail.com>
2025-09-15 19:06:18 +0900

    initial setting

--- Source/CatchMeIfYouCan/CatchMeIfYouCan.Build.cs
+++ Source/CatchMeIfYouCan/CatchMeIfYouCan.Build.cs
@@ -11,6 +11,12 @@ public class CatchMeIfYouCan : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				"CatchMeIfYouCan"
+			}
+		);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
