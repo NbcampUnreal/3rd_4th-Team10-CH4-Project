1976149fc799af002c125e9df61d58ef846e0f89
kmy9317 <kimmin9632@gmail.com>
2025-09-25 13:15:58 +0900

    [feat] 클라이언트 예측 기반 서버와의 타이머 동기화 기법을 적용한 인게임 타이머 구현

--- Source/CatchMeIfYouCan/Player/CYPlayerController.h
+++ Source/CatchMeIfYouCan/Player/CYPlayerController.h
@@ -30,8 +30,15 @@ public:
 	// PawnData 준비 알림 (PlayerState에서 호출)
 	void OnPawnDataReady();
 
+	// 동기화된 서버 시간 요청
+	float GetServerTime();
+	
+	float GetSingleTripTime() const { return SingleTripTime; }
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void ReceivedPlayer() override;
 	
 	virtual void PostProcessInput(const float DeltaTime, const bool bGamePaused) override;
 	
@@ -50,6 +57,17 @@ protected:
 	// HUD 초기화 (PawnData 기반)
 	void CreateTeamSpecificHUD(const UCYPawnData* PawnData);
 
+	// 네트워크 시간 동기화
+	void CheckNetworkTimerSync();
+	
+	// 서버측에 실제 인게임 시간 요청
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	// 클라이언트에 실제 타임 전달
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
 private:
 	// 초기화 상태 추적
 	bool bClientInitialized = false;
@@ -60,8 +78,19 @@ private:
 	// 캐시된 PawnData (팀 변경 감지용)
 	UPROPERTY()
 	const UCYPawnData* CachedPawnData = nullptr;
-	
+
+	// 초기화 재시도 횟수
 	int32 InitializationRetryCount = 0;
 	const int32 MaxRetryCount = 50;
 
+	// 클라이언트 서버간 시간 차이
+	float ClientServerDeltaTime = 0.f;
+
+	// 클라이언트 <-> 서버 간의 단일 딜레이 시간
+	float SingleTripTime = 0.f;
+
+	FTimerHandle ResyncTimerHandle;
+
+	UPROPERTY(EditDefaultsOnly, Category="CY|TimeSync", meta=(ClampMin="0.1", ClampMax="60.0"))
+	float ResyncInterval = 5.f;
 };
